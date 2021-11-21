0346bd366aa747089ef669364187474f211b00bf
김소희 <rosesh0218@gmail.com>
2021-11-21 22:23:25 +0900

    Update [JOIN]있었는데없었다.sql

--- programmers/[JOIN]있었는데없었다.sql
+++ programmers/[JOIN]있었는데없었다.sql
@@ -1,6 +1,13 @@
 -- 보호 시작일보다 입양일이 더 빠른 동물의 아이디와 이름을 조회하는 SQL문을 작성해주세요. 
 -- 이때 결과는 보호 시작일이 빠른 순으로 조회해야합니다.
 
+-- 정답
+SELECT I.ANIMAL_ID, I.NAME 
+FROM ANIMAL_INS I LEFT JOIN ANIMAL_OUTS O
+ON I.ANIMAL_ID = O.ANIMAL_ID 
+WHERE I.DATETIME > O.DATETIME 
+ORDER BY I.DATETIME
+
 -- 1
 SELECT B.ANIMAL_ID, B.NAME
 FROM ANIMAL_INS AS A, ANIMAL_OUTS AS B
