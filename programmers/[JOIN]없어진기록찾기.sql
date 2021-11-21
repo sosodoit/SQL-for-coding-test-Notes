059328718ad0d682e64a52ce38754d50df864c06
김소희 <rosesh0218@gmail.com>
2021-11-21 14:56:35 +0900

    Create [JOIN]없어진기록찾기.sql

---
+++ programmers/[JOIN]없어진기록찾기.sql
@@ -0,0 +1,6 @@
+-- 입양을 간 기록은 있는데, 보호소에 들어온 기록이 없는 동물의 ID와 이름을 ID 순으로 조회하는 SQL문을 작성해주세요.
+SELECT AO.ANIMAL_ID, AO.NAME
+FROM ANIMAL_INS AS AI
+JOIN ANIMAL_OUTS AS AO
+ON AI.ANIMAL_ID = AO.ANIMAL_ID
+WHERE AI.ANIMAL_ID IS NULL;
